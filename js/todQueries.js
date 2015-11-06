96afd7e434dc716f0bc6460cb8747a39fc0992b4
Agustín Benassi <agusbenassi@gmail.com>
2015-11-06 14:23:19 -0300

    Agrego comentario para un futuro uso de funciones más optimizadas.

--- js/todQueries.js
+++ js/todQueries.js
@@ -2,6 +2,8 @@ POLYS_UNION_SELECT = "1 AS cartodb_id, ST_MakeValid(ST_Union(ST_Intersection(ST_
 POLYS_DIFF_UNION_SELECT = "1 AS cartodb_id, ST_Union(ST_Difference(ST_MakeValid(divisiones.the_geom), buffers_union.the_geom)) AS the_geom FROM divisiones, buffers_union"
 SELECT_BUFFERS = "ST_Intersection(ST_MakeValid(buffers_estaciones.the_geom_webmercator), ST_MakeValid(divs.the_geom_webmercator)) AS the_geom_webmercator, buffers_estaciones.cartodb_id"
 
+// no logré que esto funcione, queda sólo para tratar de hacerlo en el futuro
+// http://gis.stackexchange.com/questions/50399/how-best-to-fix-a-non-noded-intersection-problem-in-postgis
 SAFE_INTERSECTION = " \
 CREATE OR REPLACE FUNCTION safe_isect(geom_a geometry, geom_b geometry) \
 RETURNS geometry AS \
